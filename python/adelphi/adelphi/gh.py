7d3ad8ae3fda81341a82c56a03f57866ebaa8253
Bret McGuire <bret.mcguire@datastax.com>
2020-11-20 17:28:16 -0600

    Updating GH interaction to use new fork + PR model

--- python/adelphi/adelphi/gh.py
+++ python/adelphi/adelphi/gh.py
@@ -20,7 +20,12 @@ import os
 import os.path
 import uuid
 
-from github import Github, InputGitAuthor
+from github import Github
+
+import logging
+import os
+import os.path
+import uuid
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger('adelphi')
@@ -28,27 +33,29 @@ log = logging.getLogger('adelphi')
 # We're assuming any storage repo will be created after the converstion to "main"
 MAIN_BRANCH_NAME = "main"
 
+ORIGIN_REPO = "schemafarce/schema-repository"
+
 def build_github(token):
-    log.info("Repository token: " + token)
     return Github(token)
 
-def build_repository(gh, repo_name):
-    log.info("Repository name: " + repo_name)
-    return gh.get_repo(repo_name)
+def build_origin_repo(gh):
+    return gh.get_repo(ORIGIN_REPO)
 
-def build_branch(gh, repo, author_name):
+def build_branch(gh, repo):
     main_branch = repo.get_branch(MAIN_BRANCH_NAME)
-    branch_name = "_".join([author_name, str(uuid.uuid4())])
+    branch_name = "_".join([gh.get_user().login, str(uuid.uuid4())])
     repo.create_git_ref(ref="refs/heads/" + branch_name, sha=main_branch.commit.sha)
     return branch_name
 
-def commit_schemas(gh, repo, schema_dir, branch_name, author_name):
-    author_obj = gh.get_user(author_name)
+def commit_schemas(gh, repo, schema_dir, branch_name):
+    curr_user_name = gh.get_user().login
     for schema in os.listdir(schema_dir):
-        repo_path = "/".join([author_obj.login, schema])
-        schema_file_path = os.path.join(schema_dir,schema)
+        repo_path = "/".join([curr_user_name, schema])
+        schema_file_path = os.path.join(schema_dir, schema)
         schema_str = open(schema_file_path).read()
-        repo.create_file(path=repo_path, message="Schema " + schema, content=schema_str, branch=branch_name, author=InputGitAuthor(author_obj.name, author_obj.email))
+        repo.create_file(path=repo_path, message="Schema " + schema, content=schema_str, branch=branch_name)
 
-def build_pull_request(repo, branch_name, author_name):
-    repo.create_pull(title = "Schemas for user " + author_name, body = "Schemas for user " + author_name, base = MAIN_BRANCH_NAME, head = branch_name)
+def build_pull_request(gh, origin_repo, branch_name):
+    curr_user_name = gh.get_user().login
+    title_str = "Schemas for user {}".format(curr_user_name)
+    origin_repo.create_pull(title = title_str, body = title_str, base = MAIN_BRANCH_NAME, head = ":".join([curr_user_name, branch_name]))
