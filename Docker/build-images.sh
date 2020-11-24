2951bcc521ebd4f0a44e0742171142bd9d69124e
Gianluca Righetto <gianluca@menttis.com>
2020-11-24 16:00:03 -0300

    #32 Added Dockerfiles for Spark, cassandra-diff and gemini; Replaced the custom images by the ones published to Dockerhub; Removed NFS for the time-being (a GKE solution will follow up next).

---
+++ Docker/build-images.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Spark comes with a Dockerfile, let's use that
+SPARK_DOCKERFILE=spark/kubernetes/dockerfiles/spark/Dockerfile
+
+function download_spark() {
+	local SPARK_DOWNLOAD_URL="https://archive.apache.org/dist/spark/spark-3.0.0/spark-3.0.0-bin-hadoop3.2.tgz"
+	mkdir spark
+	wget $SPARK_DOWNLOAD_URL -O - | tar -xz --strip-components=1 -C spark
+}
+
+# try to find Spark's Dockerfile, if it doesn't exist, download the Spark tarball
+if [ ! -f "$SPARK_DOCKERFILE" ]; then
+	download_spark
+fi
+
+docker build --tag spark:3.0.0 -f $SPARK_DOCKERFILE spark
+docker build --tag cassandra-diff:4c9bc4 cassandra-diff
+docker build --tag gemini:1.7.3 gemini
---
+++ Docker/push-images.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Dockerhub org
+REPO="adelphidev"
+
+# Push cassandra-diff image
+docker tag cassandra-diff:4c9bc4 $REPO/cassandra-diff:4c9bc4
+docker push $REPO/cassandra-diff:4c9bc4
+
+# Push gemini image
+docker tag gemini:1.7.3 $REPO/gemini:1.7.3
+docker push $REPO/gemini:1.7.3
