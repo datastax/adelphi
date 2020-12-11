08fdcecc846d7ed87e19510773b44c6faae7fff5
Bret McGuire <bret.mcguire@datastax.com>
2020-12-11 12:07:35 -0600

    Normalizing spacing.  Also starting to prove out Markdown support.

--- python/adelphi/setup.py
+++ python/adelphi/setup.py
@@ -14,35 +14,36 @@ if not PY3:
     dependencies.append('backports.tempfile ~= 1.0')
 
 long_description = ""
-with open("README.rst") as f:
+with open("README.md") as f:
     long_description = f.read()
 
 setup(
-   name='adelphi',
-   url='https://github.com/datastax/adelphi',
-   description='Tooling for Adelphi',
-   long_description=long_description,
-   version='0.1.0',
-   project_urls={
-      'Source': 'https://github.com/datastax/adelphi',
-      'Issues': 'https://github.com/datastax/adelphi/issues',
-   },
-   author='DataStax',
-   classifiers=[
-      'Development Status :: 3 - Alpha',
-      'Intended Audience :: Developers',
-      'License :: OSI Approved :: Apache Software License',
-      'Natural Language :: English',
-      'Operating System :: OS Independent',
-      'Programming Language :: Python',
-      'Programming Language :: Python :: 2.7',
-      'Programming Language :: Python :: 3.5',
-      'Programming Language :: Python :: 3.6',
-      'Programming Language :: Python :: 3.7',
-      'Programming Language :: Python :: 3.8',
-      'Programming Language :: Python :: 3.9',
-      'Programming Language :: Python :: Implementation :: CPython',
-      'Topic :: Software Development :: Libraries :: Python Modules'
+    name='adelphi',
+    url='https://github.com/datastax/adelphi',
+    description='Tooling for Adelphi',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version='0.1.0',
+    project_urls={
+        'Source': 'https://github.com/datastax/adelphi',
+        'Issues': 'https://github.com/datastax/adelphi/issues',
+    },
+    author='DataStax',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Libraries :: Python Modules'
    ],
    packages=['adelphi'],
    scripts=['bin/adelphi'],
