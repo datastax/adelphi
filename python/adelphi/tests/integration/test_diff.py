177b25ce427aa49c1c1063b0004f7abb8049915d
Bret McGuire <bret.mcguire@datastax.com>
2021-02-22 16:10:47 -0600

    Schema creation now works entirely on 4.0-beta3, at least as long as you comment out the SASI indexes

--- python/adelphi/tests/integration/test_diff.py
+++ python/adelphi/tests/integration/test_diff.py
@@ -18,37 +18,52 @@ class TestDiff(unittest.TestCase):
     def setUp(self):
         self.client = docker.from_env()
 
+
     def testSimple(self):
-        #container = self.client.containers.run("hello-world", detach=True)
-        #self.assertTrue(container in self.client.containers.list())
         self.__testCassandraVersion("4.0-beta4")
 
-    def __testCassandraVersion(self, version):
-        imageStr = "cassandra:{}".format(version)
-        logConfig = docker.types.LogConfig(type=docker.types.LogConfig.types.SYSLOG)
-        container = self.client.containers.run(name="adelphi", remove=True, detach=True, ports={9042:9042}, image=imageStr)
 
-        # We now have to wait until things are actually up
-        containerReady = False
-        while not containerReady:
-            # Old version based on literal adaptation of prior shell script, probably not applicable at all
+    def __connectToCassandra(self):
+        self.session = None
+        while not self.session:
+            # Old version based on literal adaptation of prior shell script, probably not applicable at all now
             #(exitcode,output) = container.exec_run(cmd="cqlsh -e 'select * from system.local'")
-            #log.info("exitcode: {}".format(exitcode))
-            #log.info("output: {}".format(output))
             #containerReady = (exitcode == 0)
             try:
-                cluster = Cluster(["127.0.0.1"],port=9042)
-                session = cluster.connect()
-                rs = session.execute("select * from system.local")
-                log.info(rs.one())
-                cluster.shutdown()
-                containerReady = True
+                self.cluster = Cluster(["127.0.0.1"],port=9042)
+                self.session = self.cluster.connect()
+
+                # Confirm that the session is actually functioning before calling things good
+                rs = self.session.execute("select * from system.local")
+                log.info("Connected to Cassandra cluster, first row of system.local: {}".format(rs.one()))
             except:
                 log.info("Exception connecting, will retry", exc_info=sys.exc_info()[0])
-
-            if not containerReady:
                 time.sleep(10)
 
-        log.info("C* ready")
 
-        container.stop()
+    def __createSchema(self):
+        log.info("Creating schema on Cassandra cluster")
+        with open("integration-tests/base-schema.cql") as schema:
+            for stmt in [s.strip() for s in schema.read().split(';')]:
+                if len(stmt) == 0:
+                    log.info("Skipping empty statement")
+                    continue
+                if stmt.startswith("//"):
+                    log.info("Skipping commented statement {}".format(stmt))
+                    continue
+                realStmt = stmt + ';'
+                log.info("Executing statement {}".format(realStmt))
+                self.session.execute(realStmt)
+
+
+    def __testCassandraVersion(self, version):
+        container = self.client.containers.run(name="adelphi", remove=True, detach=True, ports={9042:9042}, image="cassandra:{}".format(version))
+
+        self.__connectToCassandra()
+
+        log.info("Cassandra cluster ready")
+
+        self.__createSchema()
+
+        self.cluster.shutdown()
+        #container.stop()
