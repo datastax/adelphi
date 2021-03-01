69e903afb8ab611166be5f6f8e50e1fc90b875d3
Bret McGuire <bret.mcguire@datastax.com>
2021-03-01 17:24:50 -0600

    Re-working interaction of base class + diff test class in prep for adding new tests

--- python/adelphi/tests/integration/__init__.py
+++ python/adelphi/tests/integration/__init__.py
@@ -5,6 +5,8 @@ import time
 
 from cassandra.cluster import Cluster
 
+import docker
+
 # Default C* versions to include in all integration tests
 CASSANDRA_VERSIONS = ["2.1.22", "2.2.19", "3.0.23", "3.11.9", "4.0-beta3"]
 
@@ -29,15 +31,16 @@ class BaseIntegrationTest:
                 time.sleep(1)
 
 
-    def createSchema(self, session=None, schemaPath=None):
-        log.info("Creating schema on Cassandra cluster")
+    def createSchema(self, session=None):
+        schemaPath = self.getSchemaPath()
+        log.info("Creating schema on Cassandra cluster from file {}".format(schemaPath))
         with open(schemaPath) as schema:
             for stmt in [s.strip() for s in schema.read().split(';')]:
                 if len(stmt) == 0:
-                    log.info("Skipping empty statement")
+                    log.debug("Skipping empty statement")
                     continue
                 if stmt.startswith("//"):
-                    log.info("Skipping commented statement {}".format(stmt))
+                    log.debug("Skipping commented statement {}".format(stmt))
                     continue
                 realStmt = stmt + ';'
                 log.debug("Executing statement {}".format(realStmt))
@@ -45,3 +48,40 @@ class BaseIntegrationTest:
                     session.execute(realStmt)
                 except:
                     log.info("Exception executing statement: {}".format(realStmt), exc_info=sys.exc_info()[0])
+
+
+    def runTestForVersion(self, version=None):
+        log.info("Testing Cassandra version {}".format(version))
+
+        container = self.client.containers.run(name="adelphi", remove=True, detach=True, ports={9042:9042}, image="cassandra:{}".format(version))
+
+        (cluster,session) = (None,None)
+        try:
+            (cluster,session) = self.connectToLocalCassandra()
+            self.createSchema(session)
+            self.runTestWithSchema(version)
+        finally:
+            if cluster:
+                cluster.shutdown()
+
+            if "KEEP_CONTAINER" in os.environ:
+                log.info("KEEP_CONTAINER env var set, preserving Cassandra container 'adelphi'")
+            else:
+                container.stop()
+
+            self.cleanUpVersion(version)
+
+
+    def testVersions(self):
+        versions = CASSANDRA_VERSIONS
+        if "CASSANDRA_VERSIONS" in os.environ:
+            versions = os.environ["CASSANDRA_VERSIONS"].split(',')
+
+        log.info("Testing the following Cassandra versions: {}".format(versions))
+
+        for version in versions:
+            self.runTestForVersion(version)
+
+
+    def setUp(self):
+        self.client = docker.from_env()
--- python/adelphi/tests/integration/test_diff.py
+++ python/adelphi/tests/integration/test_diff.py
@@ -17,25 +17,21 @@ except ImportError:
 
 from collections import namedtuple
 
-import docker
-
 from tests.integration import BaseIntegrationTest, CASSANDRA_VERSIONS
 
 logging.basicConfig(filename="adelphi.log", level=logging.INFO)
 log = logging.getLogger('adelphi')
 
-DIFF_SCHEMA_PATH = "tests/integration/resources/base-schema.cql"
-
 TempDirs = namedtuple('TempDirs', 'basePath, outputPath, logPath')
 
-class TestDiff(unittest.TestCase, BaseIntegrationTest):
+class TestDiff(BaseIntegrationTest, unittest.TestCase):
 
-    def stdoutPath(self, version=None, dirs=None):
-        return os.path.join(dirs.outputPath, "{}-stdout.cql".format(version))
+    def stdoutPath(self, version=None):
+        return os.path.join(self.dirs.outputPath, "{}-stdout.cql".format(version))
 
 
-    def outputDirPath(self, version=None, dirs=None):
-        return os.path.join(dirs.outputPath, version)
+    def outputDirPath(self, version=None):
+        return os.path.join(self.dirs.outputPath, version)
 
 
     def makeTempDirs(self):
@@ -47,69 +43,49 @@ class TestDiff(unittest.TestCase, BaseIntegrationTest):
         return TempDirs(basePath, outputPath, logPath)
 
 
-    def runAdelphi(self, version=None, dirs=None):
+    def runAdelphi(self, version=None):
         log.info("Running Adelphi")
-        stdoutPath = self.stdoutPath(version, dirs)
-        outputDirPath = self.outputDirPath(version, dirs)
+        stdoutPath = self.stdoutPath(version)
+        outputDirPath = self.outputDirPath(version)
         os.mkdir(outputDirPath)
-        stderrPath = os.path.join(dirs.logPath, "{}-stderr.log".format(version))
+        stderrPath = os.path.join(self.dirs.logPath, "{}-stderr.log".format(version))
         subprocess.run("adelphi export-cql --no-metadata > {} 2>> {}".format(stdoutPath, stderrPath), shell=True)
         subprocess.run("adelphi --output-dir={} export-cql 2>> {}".format(outputDirPath, stderrPath), shell=True)
         log.info("Adelphi completed")
 
 
-    def compareSchemas(self, version=None, dirs=None):
-        referencePath = "tests/integration/resources/schemas/{}.cql".format(version)
+    def compareSchemas(self, version=None):
+        referencePath = "tests/integration/resources/diff-schemas/{}.cql".format(version)
 
-        stdoutPath = self.stdoutPath(version, dirs)
+        stdoutPath = self.stdoutPath(version)
         rv1 = subprocess.run("diff -Z {} {}".format(stdoutPath, referencePath), shell=True)
         self.assertEqual(rv1.returncode, 0)
 
         # Find the created schema underneath the output dir.  Note that this logic will have to be fixed
         # once https://github.com/datastax/adelphi/issues/106 is resolved
-        outputDirPath = self.outputDirPath(version, dirs)
+        outputDirPath = self.outputDirPath(version)
         schemas = glob.glob("{}/*/schema".format(outputDirPath))
         log.info("Found schema file in output directory, path: {}".format(schemas[0]))
         rv2 = subprocess.run("diff -Z {} {}".format(schemas[0], referencePath), shell=True)
         self.assertEqual(rv2.returncode, 0)
 
-    def runTestForVersion(self, version):
-        log.info("Testing Cassandra version {}".format(version))
-
-        container = self.client.containers.run(name="adelphi", remove=True, detach=True, ports={9042:9042}, image="cassandra:{}".format(version))
-        dirs = self.makeTempDirs()
-
-        (cluster,session) = (None,None)
-        try:
-            (cluster,session) = self.connectToLocalCassandra()
-            self.createSchema(session, DIFF_SCHEMA_PATH)
-            self.runAdelphi(version, dirs)
-            self.compareSchemas(version, dirs)
-        finally:
-            if cluster:
-                cluster.shutdown()
-
-            if "KEEP_CONTAINER" in os.environ:
-                log.info("KEEP_CONTAINER env var set, preserving Cassandra container 'adelphi'")
-            else:
-                container.stop()
-
-            if "KEEP_LOGS" in os.environ:
-                log.info("KEEP_LOGS env var set, preserving logs/output at {}".format(dirs.basePath))
-            else:
-                shutil.rmtree(dirs.basePath)
-
 
     def setUp(self):
-        self.client = docker.from_env()
+        super(TestDiff, self).setUp()
+        self.dirs = self.makeTempDirs()
 
 
-    def testDiff(self):
-        versions = CASSANDRA_VERSIONS
-        if "CASSANDRA_VERSIONS" in os.environ:
-            versions = os.environ["CASSANDRA_VERSIONS"].split(',')
+    def runTestWithSchema(self, version):
+        self.runAdelphi(version)
+        self.compareSchemas(version)
 
-        log.info("Will test the following Cassandra versions: {}".format(versions))
 
-        for version in versions:
-            self.runTestForVersion(version)
+    def cleanUpVersion(self, version):
+        if "KEEP_LOGS" in os.environ:
+            log.info("KEEP_LOGS env var set, preserving logs/output at {}".format(self.dirs.basePath))
+        else:
+            shutil.rmtree(self.dirs.basePath)
+
+
+    def getSchemaPath(self):
+        return "tests/integration/resources/diff-base-schema.cql"
