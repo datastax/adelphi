c90d1589db11055142894654314ad5529e0b08a1
Bret McGuire <bret.mcguire@datastax.com>
2021-03-19 16:45:22 -0500

    While I was already in here decided to address the bits in https://github.com/datastax/adelphi/issues/113 around _compareToReference only dealing with CQL files.  The change in this PR moves CQL-specific logic into the CQL-relevant test test_diff.py and implements similar functionality for YAML in test_nb.py.

--- python/adelphi/tests/integration/test_diff.py
+++ python/adelphi/tests/integration/test_diff.py
@@ -1,19 +1,41 @@
+import glob
+import logging
+import os
+import shutil
+import sys
+
 try:
     import unittest2 as unittest
 except ImportError:
     import unittest
 
+if os.name == 'posix' and sys.version_info[0] < 3:
+    import subprocess32 as subprocess
+else:
+    import subprocess
+
 from tests.integration import SchemaTestMixin
 from tests.util.schema_util import get_schema
+from tests.util.schemadiff import cqlDigestGenerator
+
+def digestSet(schemaFile):
+    rv = set()
+    for (_, digest) in cqlDigestGenerator(schemaFile):
+        rv.add(digest)
+    return rv
+
+
+def logCqlDigest(schemaFile, digestSet):
+    for (cql, digest) in cqlDigestGenerator(schemaFile):
+        if digest in digestSet:
+            log.info("Digest: {}, CQL: {}".format(digest,cql))
+
+log = logging.getLogger('adelphi')
 
 class TestDiff(unittest.TestCase, SchemaTestMixin):
 
     def setUp(self):
         super(TestDiff, self).setUp()
-
-
-    def getExportCommand(self):
-        return "export-cql --no-metadata"
 
 
     # TODO: Note that we currently have to disable support for SASI indexes when creating
@@ -27,5 +49,48 @@ class TestDiff(unittest.TestCase, SchemaTestMixin):
         return baseSchemaPath
 
 
-    def getReferenceSchemaPath(self, version):
-        return "tests/integration/resources/diff-schemas/{}.cql".format(version)
+    def runAdelphi(self, version=None):
+        exportCmd = "export-cql --no-metadata"
+        stdoutPath = self.stdoutPath(version)
+        stderrPath = self.stderrPath(version)
+        subprocess.run("adelphi {} > {} 2>> {}".format(exportCmd, stdoutPath, stderrPath), shell=True)
+        outputDirPath = self.outputDirPath(version)
+        os.mkdir(outputDirPath)
+        subprocess.run("adelphi --output-dir={} {} 2>> {}".format(outputDirPath, exportCmd, stderrPath), shell=True)
+
+
+    def _compareToReference(self, comparePath, version=None):
+        referencePath = "tests/integration/resources/diff-schemas/{}.cql".format(version)
+        referenceSet = digestSet(referencePath)
+        compareSet = digestSet(comparePath)
+
+        refOnlySet = referenceSet - compareSet
+        if len(refOnlySet) > 0:
+            log.info("Statements in reference file {} but not in compare file {}:".format(referencePath, comparePath))
+            logCqlDigest(referencePath, refOnlySet)
+        compareOnlySet = compareSet - referenceSet
+        if len(compareOnlySet) > 0:
+            log.info("Statements in compare file {} but not in reference file {}:".format(comparePath, referencePath))
+            logCqlDigest(comparePath, compareOnlySet)
+
+        self.assertEqual(referenceSet, compareSet)
+
+
+    def evalAdelphiOutput(self, version=None):
+
+        # Compare process for stdout
+        self._compareToReference(self.stdoutPath(version), version)
+
+        # Compare process for output dir
+        outputDirPath = self.outputDirPath(version)
+        allOutputFileName = "{}-all".format(version)
+        allOutputPath = self.outputDirPath(allOutputFileName)
+
+        outputSchemas = glob.glob("{}/*/schema".format(outputDirPath))
+        self.assertGreater(len(outputSchemas), 0)
+        with open(allOutputPath, "w+") as allOutputFile:
+            for outputSchema in outputSchemas:
+                with open(outputSchema) as outputSchemaFile:
+                    shutil.copyfileobj(outputSchemaFile, allOutputFile)
+                    allOutputFile.write("\n")
+        self._compareToReference(allOutputPath, version)
