8cc6562b9b781db3a2e241de0a0686d782a1d014
Bret McGuire <bret.mcguire@datastax.com>
2021-04-02 16:14:36 -0500

    Making scenario name parameterized (per feedback on code review)

--- python/adelphi/adelphi/nb.py
+++ python/adelphi/adelphi/nb.py
@@ -166,7 +166,7 @@ class NbExporter(BaseExporter):
         """Really more of a config than a schema, but we'll allow it"""
         root = {}
 
-        root["scenarios"] = {"default":[self.__get_rampup_scenario(), self.__get_main_scenario()]}
+        root["scenarios"] = {"TEMPLATE(scenarioname,default)":[self.__get_rampup_scenario(), self.__get_main_scenario()]}
         
         root["bindings"] = self.__build_bindings(self.table)
 
