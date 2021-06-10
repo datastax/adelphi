21ef1c2cfd0b022a4bb9a61bcf7d0bfae3906e77
Bret McGuire <bret.mcguire@datastax.com>
2021-06-10 00:27:12 -0500

    Add click for handling CLI args

--- python/adelphi/bin/run-adelphi-tests.py
+++ python/adelphi/bin/run-adelphi-tests.py
@@ -7,16 +7,16 @@
 # nice side effect of moving a lot of C* checking/session code out of the test
 # suite, which in turn should allow us to write simpler tests.
 import configparser
-import logging
 import os
 
+import click
 import tox
 
 import docker
 from tenacity import retry, stop_after_attempt, wait_fixed
 
 # Default C* versions to include in all integration tests
-CASSANDRA_VERSIONS = ["2.1.22", "2.2.19", "3.0.23", "3.11.9", "4.0-rc1"]
+DEFAULT_CASSANDRA_VERSIONS = ["2.1.22", "2.2.19", "3.0.23", "3.11.9", "4.0-rc1"]
 
 TOX_DEPENDENCIES = """pytest
     subprocess32 ~= 3.5"""
@@ -28,28 +28,31 @@ def getContainer(client, version):
     return client.containers.run(name="adelphi", remove=True, detach=True, ports={9042: 9042}, image="cassandra:{}".format(version))
 
 
-def getCassandraVersions():
-    if "CASSANDRA_VERSIONS" in os.environ:
-        return [s.strip() for s in os.environ["CASSANDRA_VERSIONS"].split(',')]
-    else:
-        return CASSANDRA_VERSIONS 
-
-
 def writeToxIni(version):
     config = configparser.ConfigParser()
     config["tox"] = { "envlist": "py2, py3" }
     envs = {"CASSANDRA_VERSION": version}
     config["testenv"] = {"deps": TOX_DEPENDENCIES, \
         "commands": "pytest {posargs}", \
-        "setenv": """CASSANDRA_VERSION = {}
-        KEEP_LOGS=1""".format(version)}
+        "setenv": "CASSANDRA_VERSION = {}".format(version)}
     with open(TOX_CONFIG, 'w') as configfile:
         config.write(configfile)
 
-
-if __name__ == '__main__':
+@click.command()
+@click.option('--cassandra', '-c', multiple=True, type=str)
+@click.option('--python', '-p', multiple=True, type=click.Choice(["py2","py3"], case_sensitive = False))
+@click.option("--pytest", "-t", type=str, help="Arguments to be passed to pytest")
+def runtests(cassandra, python, pytest):
     client = docker.from_env()
-    for version in getCassandraVersions():
+    tox_args = ["-e {}".format(py) for py in python] if python else []
+    if pytest:
+        tox_args.append("--")
+        tox_args.append(pytest)
+    print("Full tox args: {}".format(tox_args))
+
+    cassandra_versions = cassandra or DEFAULT_CASSANDRA_VERSIONS
+    print("Cassandra versions to test: {}".format(','.join(cassandra_versions)))
+    for version in cassandra_versions:
 
         print("Running test suite for Cassandra version {}".format(version))
         container = getContainer(client, version)
@@ -61,13 +64,14 @@ if __name__ == '__main__':
             # cmdline() will raise SystemExit when it's done so trap that here to avoid
             # exiting all the things
             try:
-                tox.cmdline()
+                tox.cmdline(tox_args)
             except SystemExit:
                 pass
         except Exception as exc:
             print("Exception running tests for Cassandra version {}".format(version), exc)
         finally:
-            if "KEEP_CONTAINER" in os.environ:
-                print("KEEP_CONTAINER env var set, preserving Cassandra container 'adelphi'")
-            else:
-                container.stop()
+            container.stop()
+
+
+if __name__ == '__main__':
+    runtests(obj={})
