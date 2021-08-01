5bd87c0ff1aed6c39d57558cbd804eccbfef6525
Gianluca Righetto <gianluca@menttis.com>
2021-08-01 03:54:06 -0300

    #162 New NodeJS module for serving an API that can be called by clients to obtain results summary, results tarball, etc.

---
+++ api/src/api/index.js
@@ -0,0 +1,64 @@
+import express from "express";
+import _ from "lodash";
+import { readDataFile } from "../utils";
+
+/*
+  Extracts key metrics of the Gemini results.
+*/
+function geminiSummary(gemini) {
+  return _.isEmpty(gemini) ? {} : {
+      write_ops: gemini.result.write_ops,
+      write_errors: gemini.result.write_errors,
+      read_ops: gemini.result.read_ops,
+      read_errors: gemini.result.read_errors
+    };
+}
+
+/*
+  Extracts key metrics of the cass-diff results.
+*/
+function cassDiffSummary(cassdiff) {
+  return _.isEmpty(cassdiff) ? {} : {
+      matched_partitions: cassdiff.matched_partitions,
+      matched_rows: cassdiff.matched_rows,
+      matched_values: cassdiff.matched_values,
+      mismatched_partitions: cassdiff.mismatched_partitions,
+      mismatched_values: cassdiff.mismatched_values,
+      partitions_only_in_source: cassdiff.partitions_only_in_source,
+      partitions_only_in_target: cassdiff.partitions_only_in_target
+    };
+}
+
+/*
+  Returns the count of all errors in the results.
+*/
+function checkErrors(results) {
+  return results.gemini.write_errors +
+         results.gemini.read_errors +
+         results.cassandra_diff.mismatched_partitions +
+         results.cassandra_diff.mismatched_values;
+}
+
+/*
+  Returns key metrics of the
+  generated results files.
+*/
+export function resultsSummary(req, res) {
+  /* Base path for the results file */
+  const DATA_PATH = process.env.DATA_PATH;
+
+  const result = {
+    gemini: geminiSummary(readDataFile(DATA_PATH + "/gemini/results.json")),
+    cassandra_diff: cassDiffSummary(readDataFile(DATA_PATH + "/cassandra-diff/job_results.json"))
+  };
+  
+  const errorCount = checkErrors(result);
+  const code = errorCount > 0 ? 500 : 200;
+  res.status(code).send(result);
+}
+
+var app = express();
+
+app.get('/results/summary', resultsSummary);
+
+export default app;
---
+++ api/tests/api.int.test.js
@@ -0,0 +1,18 @@
+import supertest from "supertest";
+import api from "../src/api";
+
+describe("GET /results/summary", () => {
+
+  test("It should return 200 status code for successful results", async () => {
+  	process.env.DATA_PATH = './tests/resources/results/without_failures';
+    const response = await supertest(api).get("/results/summary");
+    expect(response.statusCode).toBe(200);
+  });
+
+  test("It should return 500 status code for result failures", async () => {
+  	process.env.DATA_PATH = './tests/resources/results/with_failures';
+    const response = await supertest(api).get("/results/summary");
+    expect(response.statusCode).toBe(500);
+  });
+
+});
